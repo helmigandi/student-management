bb1d989106d497575f5bc0773f4554f7c6fdcc37
Helmi Sugandi <sugandihelmi@gmail.com>
2022-03-24 10:04:30 +0700

    create model student

---
+++ src/main/java/com/helmigandi/app/student/Student.java
@@ -0,0 +1,16 @@
+package com.helmigandi.app.student;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
