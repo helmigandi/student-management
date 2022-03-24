3e0b7dd5be46445729f973ad0f65d73295c299f2
Helmi Sugandi <sugandihelmi@gmail.com>
2022-03-25 02:02:52 +0700

    add refatch after add student

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { getAllStudents } from "./client";
-import { Layout, Menu, Breadcrumb, Table, Spin, Empty } from "antd";
+import {useState, useEffect} from "react";
+import {getAllStudents} from "./client";
+import {Layout, Menu, Breadcrumb, Table, Spin, Empty, Button, Badge, Avatar} from "antd";
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -8,13 +8,32 @@ import {
   TeamOutlined,
   UserOutlined,
   LoadingOutlined,
+  PlusOutlined,
 } from "@ant-design/icons";
+import StudentDrawerForm from "./StudentDrawerForm";
 import "./App.css";
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
+
+const TheAvatar = ({name}) => {
+  let trim = name.trim();
+  if(trim.length === 0) {
+    return <Avatar icon={<UserOutlined/>}/>
+  }
+  const split = trim.split(" ");
+  if (split.length === 1) {
+    return <Avatar>{name.charAt(0)}</Avatar>
+  }
+  return <Avatar>{`${name.charAt(0)}${name.charAt(name.length - 1)}`}</Avatar>
+}
 
 const columns = [
+  {
+    title: "",
+    dataIndex: "",
+    render: (text, student) => <TheAvatar name={student.name}/>
+  },
   {
     title: "Id",
     dataIndex: "id",
@@ -33,12 +52,13 @@ const columns = [
   },
 ];
 
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
   const [students, setStudents] = useState([]);
   const [collapsed, setCollapsed] = useState(false);
   const [fetching, setFetching] = useState(true);
+  const [showDrawer, setShowDrawer] = useState(false);
 
   useEffect(() => {
     fetchStudents();
@@ -55,63 +75,79 @@ function App() {
 
   const renderStudents = () => {
     if (fetching) {
-      return <Spin indicator={antIcon} />;
+      return <Spin indicator={antIcon}/>;
     }
     if (students.length <= 0) {
-      return <Empty />;
+      return <Empty/>;
     }
     return (
-      <Table
-        dataSource={students}
-        columns={columns}
-        rowKey={(student) => student.id}
-        bordered
-        title={() => "Students"}
-        scroll={{ y: 240 }}
-      />
+      <>
+        <StudentDrawerForm
+          showDrawer={showDrawer}
+          setShowDrawer={setShowDrawer}
+          fetchStudents={fetchStudents}
+        />
+        <Table
+          dataSource={students}
+          columns={columns}
+          rowKey={(student) => student.id}
+          bordered
+          title={() =>
+            <>
+              <Button
+                onClick={() => setShowDrawer(!showDrawer)}
+                type="primary" shape="round" icon={<PlusOutlined/>} size="small">
+                Add New Student
+              </Button>
+              <Badge count={students.length} className="site-badge-count-4"/>
+            </>
+          }
+          scroll={{y: 240}}
+        />
+      </>
     );
   };
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
+    <Layout style={{minHeight: "100vh"}}>
       <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-        <div className="logo" />
+        <div className="logo"/>
         <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
+          <Menu.Item key="1" icon={<PieChartOutlined/>}>
             Option 1
           </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
+          <Menu.Item key="2" icon={<DesktopOutlined/>}>
             Option 2
           </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+          <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
             <Menu.Item key="3">Tom</Menu.Item>
             <Menu.Item key="4">Bill</Menu.Item>
             <Menu.Item key="5">Alex</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+          <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
             <Menu.Item key="6">Team 1</Menu.Item>
             <Menu.Item key="8">Team 2</Menu.Item>
           </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
+          <Menu.Item key="9" icon={<FileOutlined/>}>
             Files
           </Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
+        <Header className="site-layout-background" style={{padding: 0}}/>
+        <Content style={{margin: "0 16px"}}>
+          <Breadcrumb style={{margin: "16px 0"}}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
           <div
             className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
+            style={{padding: 24, minHeight: 360}}
           >
             {renderStudents()}
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
+        <Footer style={{textAlign: "center"}}>
           Students Management Design ©2022
         </Footer>
       </Layout>
