6c06bee2be43c1a7aadd4c02f4f4932be232c256
Helmi Sugandi <sugandihelmi@gmail.com>
2022-03-25 00:26:21 +0700

    add get all students from db

--- src/main/java/com/helmigandi/app/student/StudentController.java
+++ src/main/java/com/helmigandi/app/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.helmigandi.app.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student(1L, "Andi Rahmat", "andi31@mail.com", Gender.MALE),
-                new Student(2L, "Suryani", "yans9@mail.com", Gender.FEMALE),
-                new Student(3L, "Anthony", "anthonz@mail.com", Gender.OTHER)
-        );
+        return studentService.getAllStudents();
     }
 }
