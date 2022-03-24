4db1f224c382d51884c85878a10127c1af8003f8
Helmi Sugandi <sugandihelmi@gmail.com>
2022-03-25 05:19:43 +0700

    add validation

--- src/main/java/com/helmigandi/app/student/Student.java
+++ src/main/java/com/helmigandi/app/student/Student.java
@@ -3,6 +3,9 @@ package com.helmigandi.app.student;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -22,9 +25,15 @@ public class Student {
             generator = "student_sequence",
             strategy = GenerationType.SEQUENCE)
     private Long id;
+    @NotBlank
+    @Column(nullable = false)
     private String name;
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
     public Student(String name, String email, Gender gender) {
